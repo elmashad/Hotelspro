b60405ed6575a9264da2bca1ce669ebf9b113aa2
elmashad <osama.elmashad@tajawal.com>
2018-01-02 00:56:39 +0200

    add more files

--- examples/search.js
+++ examples/search.js
@@ -1,4 +1,4 @@
-const HotelPro = require('./../lib/index');
+const HotelPro = require('./../lib/hotels-pro');
 
 const credentials = {
     username: 'CarsolizeCoral',
@@ -7,21 +7,21 @@ const credentials = {
 const hotelPro = new HotelPro(credentials, 'https://api-test.hotelspro.com/api/v2/search/');
 
 
-// const data =  hotelPro.getHotelClient().search({
-//         'checkInDate': '',
-//         'checkoutDate': '',
-//         'cityId' : '',
-//         'rooms': [
-//             {
-//                 'adults': 2,
-//                 'kids': 1
-//             },
-//             {
-//                 'adults': 3,
-//                 'kids': 2
-//             }
-//         ]
-//
-//     }).then(dd => {
-//         console.log(dd);
-// });
+const data =  hotelPro.search({
+        'checkInDate': '',
+        'checkoutDate': '',
+        'cityId' : '',
+        'rooms': [
+            {
+                'adults': 2,
+                'kids': 1
+            },
+            {
+                'adults': 3,
+                'kids': 2
+            }
+        ]
+
+    }).then(dd => {
+        console.log(dd);
+});
